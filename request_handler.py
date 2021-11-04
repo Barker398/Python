52bb596b735d36906817b28c5b36cee481af7503
sam barker <sam_barker@LAPTOP-T8ER9AMA.localdomain>
2021-11-04 15:41:19 -0500

    finished ch.10

--- request_handler.py
+++ request_handler.py
@@ -3,6 +3,8 @@ import json
 from animals import (
     get_all_animals,
     get_single_animal,
+    get_animals_by_location_id,
+    get_animals_by_status,
     create_animal,
     delete_animal,
     update_animal   
@@ -11,6 +13,7 @@ from animals import (
 from customers import (
     get_all_customers, 
     get_single_customer,
+    get_customers_by_email,
     create_customer,
     delete_customer,
     update_customer
@@ -19,6 +22,7 @@ from customers import (
 from employees import (
     get_all_employees,
     get_single_employee,
+    get_employees_by_location_id,
     create_employee, 
     delete_employee,
     update_employee
@@ -41,24 +45,36 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     def parse_url(self, path):
         # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
+        # path is "/customers/1", the resulting list will
+        # have "" at index 0, "customers" at index 1, and "1"
         # at index 2.
         path_params = path.split("/")
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
-        try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
 
-        return (resource, id)  # This is a tuple
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /customers
+            except ValueError:
+                pass  # Request had trailing slash: /customers/
+
+            return (resource, id)
 
     # Here's a class function
     def _set_headers(self, status):
@@ -80,36 +96,57 @@ class HandleRequests(BaseHTTPRequestHandler):
         self._set_headers(200)
         response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
-        
-        if resource == "animals":
-            if id is not None:
-                response = f"{get_single_animal(id)}"
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
 
-            else:
-                response = f"{get_all_animals()}"
-        
-        elif resource == "locations":
+        # Response from parse_url() is a tuple with 2
+        # items in it, which means the request was for
+        # `/animals` or `/animals/2`
+        if len(parsed) == 2:
+            ( resource, id ) = parsed
+
+            if resource == "animals":
+                if id is not None:
+                    response = f"{get_single_animal(id)}"
+                else:
+                    response = f"{get_all_animals()}"
+            elif resource == "customers":
+                if id is not None:
+                    response = f"{get_single_customer(id)}"
+                else:
+                    response = f"{get_all_customers()}"
+            elif resource == "employees":
+                if id is not None:
+                    response = f"{get_single_employee(id)}"
+                else:
+                    response = f"{get_all_employees()}"
+            elif resource == "locations":
                 if id is not None:
                     response = f"{get_single_locaton(id)}"
-
                 else:
                     response = f"{get_all_locations()}"
 
-        elif resource == "employees":
-                if id is not None:
-                    response = f"{get_single_employee(id)}"
+        # Response from parse_url() is a tuple with 3
+        # items in it, which means the request was for
+        # `/resource?parameter=value`
+        # http://localhost:8088/animals?location_id=1
+        elif len(parsed) == 3:
+            ( resource, key, value ) = parsed
 
-                else:
-                    response = f"{get_all_employees()}"
+            # Is the resource `customers` and was there a
+            # query parameter that specified the customer
+            # email as a filtering value?
+            if key == "email" and resource == "customers":
+                response = get_customers_by_email(value)
 
-        elif resource == "customers":
-                if id is not None:
-                    response = f"{get_single_customer(id)}"
+            elif key == "location_id" and resource == "animals":
+                response = get_animals_by_location_id(value)
 
-                else:
-                    response = f"{get_all_customers()}"
+            elif key == "status" and resource == "animals":
+                response = get_animals_by_status(value)
+
+            elif key == "location_id" and resource == "employees":
+                response = get_employees_by_location_id(value)
 
         self.wfile.write(response.encode())
         
